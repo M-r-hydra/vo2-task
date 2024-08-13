a4d1f69d003e865e6c949edd666cfb9a25e52bd1
Ali Goodarzi <me74242@gmail.com>
2024-08-13 10:49:15 +0330

    @apply provider+router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom/client";
+// React
+
+// Components
 import App from "./App";
+// Components
+
+// Redux
+import { Provider } from "react-redux";
+import { store } from "./StateManagers/RTK/store";
+// Redux
+
+// Router
+import { BrowserRouter as Router } from "react-router-dom";
+// Router
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -8,6 +22,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
